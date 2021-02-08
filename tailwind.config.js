bda66fbbc0c462814d64122c0e147e493a3946ec
Vincent Wu <vincent.wu@1health.fr>
2021-02-08 14:29:02 +0100

    Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,57 @@
+const colors = require('tailwindcss/colors');
+const typography = require('@tailwindcss/typography');
+
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
-  theme: {
-    extend: {},
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './index.html',
+      './pages/**/*.md',
+      './src/**/*.vue',
+      './src/**/*.md',
+      './src/**/*.js',
+      './src/**/*.ts',
+    ],
+    options: {
+      safelist: ['prose', 'prose-sm', 'm-auto'],
+    },
   },
+  important: true,
   variants: {
-    extend: {},
+    cursor: ['responsive', 'disabled'],
+    backgroundColor: ['dark', 'hover', 'disabled'],
+    borderColor: ['dark', 'active', 'focus', 'disabled'],
+    textColor: ['dark', 'hover', 'active', 'disabled'],
+    opacity: ['dark', 'hover', 'active', 'focus', 'disabled'],
   },
-  plugins: [],
-}
+  darkMode: 'class',
+  plugins: [typography],
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+      },
+      fontFamily: {
+        sans:
+          '"Inter", Inter var,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: 'var(--fg)',
+            a: { color: 'var(--fg-deeper)' },
+            b: { color: 'var(--fg-deep)' },
+            code: { color: 'var(--fg-deep)' },
+            strong: { color: 'var(--fg-deep)' },
+            em: { color: 'inherit' },
+            h1: { color: 'var(--fg-deeper)' },
+            h2: { color: 'var(--fg-deep)' },
+            h3: { color: 'inherit' },
+            h4: { color: 'inherit' },
+            blockquote: { color: 'inherit' },
+          },
+        },
+      },
+    },
+  },
+};
