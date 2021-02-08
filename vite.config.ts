9e6443ea9c75b1eb9f13451df5c4b137c94dbd11
Vincent Wu <vincent.wu@1health.fr>
2021-02-08 17:07:05 +0100

    Rename icons

--- vite.config.ts
+++ vite.config.ts
@@ -13,32 +13,32 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/android-launchericon-48-48.png',
+            src: '/android-launchericon-48.png',
             sizes: '48x48',
             type: 'image/png',
           },
           {
-            src: '/android-launchericon-72-72.png',
+            src: '/android-launchericon-72.png',
             sizes: '72x72',
             type: 'image/png',
           },
           {
-            src: '/android-launchericon-96-96.png',
+            src: '/android-launchericon-96.png',
             sizes: '96x96',
             type: 'image/png',
           },
           {
-            src: '/android-launchericon-144-144.png',
+            src: '/android-launchericon-144.png',
             sizes: '144x144',
             type: 'image/png',
           },
           {
-            src: '/android-launchericon-192x192.png',
+            src: '/android-launchericon-192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-launchericon-512x512.png',
+            src: '/android-launchericon-512.png',
             sizes: '512x512',
             type: 'image/png',
           },
