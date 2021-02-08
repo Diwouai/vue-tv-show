9fcc8b9cab1e669e1b62e044ff62143d2fa5b760
Vincent Wu <vincent.wu@1health.fr>
2021-02-08 16:26:39 +0100

    Add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), VitePWA()],
+});
